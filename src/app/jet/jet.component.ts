96525ea95f32809212a98cb07ceaa0a7a4e750b3
Modeste ASSIONGBON <mo.assiongbon@gmail.com>
2023-12-02 10:53:40 +0100

    [deferable view] Before implementation

---
+++ src/app/jet/jet.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-jet',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p class="comp-jet">🚀 Let's Fly !!!</p>`,
+})
+export class JetComponent {}
