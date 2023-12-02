07db57e5c1a794413d5cb04fe60e0400495de5ac
Modeste ASSIONGBON <mo.assiongbon@gmail.com>
2023-12-02 10:10:39 +0100

    [new control flow] After Implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+type Level = 'J' | 'M' | 'S';
+interface UserInfos {
+  name: string;
+  level: Level;
+}
+
+const initialValue: UserInfos[] = [
+  {
+    name: 'Toto',
+    level: 'M',
+  },
+  {
+    name: 'Ali',
+    level: 'S',
+  },
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ng_17';
+export class AppComponent implements OnInit {
+  level: Level | null = null;
+  users: UserInfos[] = [];
+
+  ngOnInit(): void {
+    this.initList();
+  }
+
+  clearList() {
+    this.users = [];
+  }
+
+  initList() {
+    this.users = initialValue;
+  }
 }
