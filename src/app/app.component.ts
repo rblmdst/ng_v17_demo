95e541c7fdb8d07dcac1ec5d0e4a44896a1adc8f
Modeste ASSIONGBON <mo.assiongbon@gmail.com>
2024-04-14 15:12:47 +0200

    [deferrable view][Devoxx] Before Implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { JetComponent } from './jet/jet.component';
@@ -19,4 +19,10 @@ import { LoadingComponent } from './loading/loading.component';
     LoadingComponent,
   ],
 })
-export class AppComponent {}
+export class AppComponent {
+  isOk = signal(false);
+
+  onTargetClicked() {
+    this.isOk.set(true);
+  }
+}
